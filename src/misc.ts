28a90b28630cf9a31ca1eaa8e15239b6da359e85
Pig Fang <g-plane@hotmail.com>
2020-09-18 23:01:40 +0800

    improve `lazy` parser

--- src/misc.ts
+++ src/misc.ts
@@ -2,16 +2,18 @@ import type { IParser, Input } from "./core.ts";
 
 interface LazyParser<O, E, I extends Input> extends IParser<O, E, I> {
   fn: () => IParser<O, E, I>;
+  parser: IParser<O, E, I> | null;
 }
 export function lazy<O, E, I extends Input>(
   fn: () => IParser<O, E, I>,
 ): LazyParser<O, E, I> {
   return {
     fn,
+    parser: null,
     parse(input) {
-      const { fn } = this;
+      this.parser ??= this.fn();
 
-      return fn().parse(input);
+      return this.parser.parse(input);
     },
   };
 }
