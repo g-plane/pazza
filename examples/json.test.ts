d746f715a2a8de7654a810cfd96d17cf251b3413
Pig Fang <g-plane@hotmail.com>
2020-09-18 23:19:52 +0800

    add "JSON parser" example

---
+++ examples/json.test.ts
@@ -0,0 +1,36 @@
+import { assertEquals } from "https://deno.land/std@0.69.0/testing/asserts.ts";
+import { jsonValue } from "./json.ts";
+
+Deno.test("jsonParser", () => {
+  const result = jsonValue().parse(`
+    [
+      2,
+      -43.21,
+      true,
+      "test",
+      "\\n\\"\\t",
+      false,
+      {
+        "a": "b",
+        "c": null
+      }
+    ]
+  `);
+
+  assertEquals(result, {
+    ok: true,
+    input: "\n  ",
+    output: [
+      2,
+      -43.21,
+      true,
+      "test",
+      '\n"\t',
+      false,
+      {
+        "a": "b",
+        "c": null,
+      },
+    ],
+  });
+});
---
+++ examples/json.ts
@@ -0,0 +1,136 @@
+import {
+  IParser,
+  between,
+  many0,
+  many1,
+  char,
+  noneOfChars,
+  escapedWith,
+  digit,
+  sepBy,
+  string,
+  trim,
+  map,
+  or,
+  optional,
+  choice,
+  serial,
+  lazy,
+} from "../mod.ts";
+
+function jsonBoolean(): IParser<boolean> {
+  return or(
+    map(string("true"), () => true),
+    map(string("false"), () => false),
+  );
+}
+
+function jsonNull(): IParser<null> {
+  return map(string("null"), () => null);
+}
+
+function jsonNumber(): IParser<number> {
+  const sign = map(optional(char("-")), (s) => s ?? "");
+  const digits = map(many1(digit()), (list) => list.join(""));
+  const parts = serial(
+    sign,
+    digits,
+    optional(map(
+      serial(char("."), digits),
+      ([_, float]) => float,
+    )),
+  );
+
+  return map(parts, ([sign, int, float]) => {
+    if (float) {
+      return Number.parseFloat(`${sign}${int}.${float}`);
+    } else {
+      return Number.parseInt(`${sign}${int}`);
+    }
+  });
+}
+
+function jsonString(): IParser<string> {
+  return map(
+    between(
+      char('"'),
+      char('"'),
+      many0(choice(
+        noneOfChars('"', "\\"),
+        escapedWith("\\", [
+          ['"', '"'],
+          ["\\", "\\"],
+          ["/", "/"],
+          ["b", "\b"],
+          ["f", "\f"],
+          ["n", "\n"],
+          ["r", "\r"],
+          ["t", "\t"],
+        ]),
+      )),
+    ),
+    (chars) => chars.join(""),
+  );
+}
+
+function jsonArray(): IParser<JsonValue[]> {
+  return between(
+    char("["),
+    trim(char("]")),
+    trim(sepBy(trim(char(",")), jsonValue())),
+  );
+}
+
+function jsonProperty(): IParser<[string, JsonValue]> {
+  return map(
+    serial(trim(jsonString()), trim(char(":")), jsonValue()),
+    ([key, _, value]) => [key, value],
+  );
+}
+
+function jsonObject(): IParser<{ [x in string]: JsonValue }> {
+  return map(
+    between(
+      char("{"),
+      trim(char("}")),
+      trim(sepBy(trim(char(",")), jsonProperty())),
+    ),
+    Object.fromEntries,
+  );
+}
+
+type JsonValue =
+  | string
+  | number
+  | boolean
+  | null
+  | { [x in string]: JsonValue }
+  | JsonValue[];
+
+export function jsonValue(): IParser<JsonValue> {
+  return lazy(() =>
+    trim(choice(
+      jsonBoolean(),
+      jsonNull(),
+      jsonString(),
+      jsonObject(),
+      jsonNumber(),
+      jsonArray(),
+    ))
+  );
+}
+
+console.log(
+  jsonValue().parse(`
+    [
+      2,
+      true,
+      "test",
+      false,
+      {
+        "a": "b",
+        "c": null
+      }
+    ]
+`),
+);
